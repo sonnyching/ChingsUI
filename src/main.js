f0c13d8063fbe2e9be798ff073dec93dab4a37f4
sonnyching <sonnyching@outlook.com>
2017-10-15 20:23:28 +0800

    开启history模式，进行了一些配置上的修改； 添加了百度统计代码；

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import '../statics/common/common.css'
 import URL from '../src/utils/Interface'
 import HTTP from '../src/utils/httputil'
 import 'element-ui/lib/theme-default/index.css'
+// import '../statics/js/baidu_statics'
 // import { Button, Select, Row, Col, Input, Menu,
 //   Submenu, MenuItem, MenuItemGroup, Option,
 //   TableColumn, Table, Tooltip, Pagination, Loading, Dialog} from 'element-ui'
@@ -51,7 +52,8 @@ Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
 const routers = new VueRouter({
-  routes: router
+  routes: router,
+  mode: 'history'
 })
 
 //  导航钩子，用作登陆校验
@@ -61,6 +63,18 @@ routers.beforeEach((to, from, next) => {
   // console.log(sessionStorage.getItem('ChingsToken'))
   // console.log(sessionStorage.getItem('ChingsToken') !== null)
 
+  //  百度统计
+  if (from.name) {
+    // console.log('111-' + to.fullPath)
+    /* eslint-disable */
+    _hmt.push(['_trackPageview', '/#' + to.fullPath, window.location.origin])
+  } else {
+    // console.log('2-' + to.fullPath)
+    /* eslint-disable */
+    _hmt.push(['_trackPageview', '/#' + to.fullPath])
+  }
+
+  // 后台权限拦截
   if (to.path.indexOf('supervisor') >= 0) {
     var token = sessionStorage.getItem('ChingsToken')
     console.log(token)
